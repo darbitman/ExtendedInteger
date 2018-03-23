939b2be52ed6ab1fef69bfe86ca301427294e561
unknown <d.arbitman@gmail.com>
2018-03-23 14:42:53 -0700

    Adding cmake support

--- ExtendedMath/ExtendedInteger.h
+++
@@ -1,9 +0,0 @@
-#ifndef _EXTENDEDINTEGER_H_
-#define _EXTENDEDINTEGER_H_
-
-#include "UnsignedExtendedInt.h"
-#include "SignedExtendedInt.h"
-#include "typedefs.h"
-
-
-#endif
---
+++ include/ExtendedInteger.h
@@ -0,0 +1,10 @@
+#ifndef _EXTENDEDINTEGER_H_
+#define _EXTENDEDINTEGER_H_
+
+
+#include "UnsignedExtendedInt.h"
+#include "SignedExtendedInt.h"
+#include "typedefs.h"
+
+
+#endif
