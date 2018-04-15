ac144581176c96ef4919d7d73383e7816ca42252
unknown <d.arbitman@gmail.com>
2018-04-15 08:31:21 -0700

    Forbid copy constructor for DirectedDFS

--- include/directedDFS/DirectedDFS.h
+++ include/directedDFS/DirectedDFS.h
@@ -5,8 +5,10 @@
 class DirectedDFS {
 private:
   bool* marked;
-  void dfs(Digraph* g, unsigned int v);            // recursive depth first search call
+  void dfs(Digraph* g, unsigned int v);             // recursive depth first search call
+  
 public:
+  DirectedDFS(const DirectedDFS& rhs) = delete;     // forbid copy constructor
   DirectedDFS(Digraph* g, unsigned int src);
   DirectedDFS(Digraph* g, const Bag<unsigned int>& srcs);
   ~DirectedDFS();
